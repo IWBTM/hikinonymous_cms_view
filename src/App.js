0b3d4352a479e90daa5561153f6d94eb133f39f1
GANG <rnlduadl2000@naver.com>
2024-06-30 02:13:52 +0900

    로그인 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 // css
-import './assets/vendor/fonts/boxicons.css';
-import './assets/vendor/css/core.css';
-import './assets/vendor/css/theme-default.css';
-import './assets/css/demo.css';
-import './assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css';
-import './assets/vendor/libs/apex-charts/apex-charts.css';
-
-// js
-import './assets/vendor/js/helpers.js';
-import './assets/vendor/libs/jquery/jquery.js';
-import './assets/vendor/libs/popper/popper.js';
-import './assets/vendor/js/bootstrap.js';
-import './assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js';
-import './assets/vendor/js/menu.js';
-import './assets/vendor/libs/apex-charts/apexcharts.js';
-import './assets/js/bootstrap.js';
-import './assets/js/helpers.js';
-import './assets/js/menu.js';
-import './assets/js/main.js';
-import './assets/js/config.js';
-import './assets/js/dashboards-analytics.js';
+import './assets/css/pages/page-auth.css';
+import './assets/css/core.css';
+import './assets/css/theme-default.css';
 
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import DashboardPage from "./pages/DashboardPage";
+import Main1 from "./pages/Main1";
 import Main2 from "./pages/Main2";
 import NotFound from "./pages/NotFound";
 
@@ -43,7 +25,7 @@ function App() {
                     <Route path="*" element={
                         <PrivateRoute isLoggedIn={isLoggedIn}>
                             <Routes>
-                                <Route path="/cms/dashboard" element={<DashboardPage/>}/>
+                                <Route path="/main1" element={<Main1/>}/>
                                 <Route path="/main2" element={<Main2/>}/>
                                 <Route path="*" element={<NotFound/>}></Route>
                             </Routes>
---
+++ src/pages/Main1.jsx
@@ -0,0 +1,9 @@
+const Main1 = (props) => {
+    return (
+        <>
+            <h3>메인 1</h3>
+        </>
+    )
+}
+
+export default Main1;
