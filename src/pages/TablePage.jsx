cbec55c08ad03e7016c71dad8b509481263e9c45
GANG <rnlduadl2000@naver.com>
2024-07-01 14:22:55 +0900

    페이지네이션 1차 추가

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,9 +1,10 @@
 import MenuTitle from "./layout/MenuTitle";
 import {useEffect, useState} from "react";
 import api from "../api/api";
+import Pagination from "./layout/Pagination";
 
 const TablePage = ({leftMenuInfo, columnList, filePath}) => {
-    const [ tabletResult, setTableResultList ] = useState({});
+    const [ tableResult, setTableResultList ] = useState({});
 
     useEffect(() => {
         const getTableResultList = async () => {
@@ -24,9 +25,9 @@ const TablePage = ({leftMenuInfo, columnList, filePath}) => {
     }
 
     const renderRows = () => {
-        if (tabletResult.totalElements > 0) {
-            return tabletResult.content.map((row, index) => {
-                let rowNum = (tabletResult.totalElements - tabletResult.totalPages) * (tabletResult.number + index + 1);
+        if (!tableResult.empty && tableResult.content) {
+            return tableResult.content.map((row, index) => {
+                let rowNum = (tableResult.totalElements - tableResult.totalPages) * (tableResult.number + index + 1);
                 return <tr>
                     <th scope="row">{rowNum}</th>
                     <td>{row.managerNm}</td>
@@ -47,18 +48,20 @@ const TablePage = ({leftMenuInfo, columnList, filePath}) => {
                 <h5 className="card-header">{leftMenuInfo.childNm.substring(0, leftMenuInfo.childNm.lastIndexOf('관리')) + " "}</h5>
                 <div className="table-responsive text-nowrap">
                     <table className="table">
-                    <thead>
-                            <tr className="text-nowrap">
-                                <th>#</th>
-                                {renderColumn()}
-                            </tr>
+                        <thead>
+                        <tr className="text-nowrap">
+                            <th>#</th>
+                            {renderColumn()}
+                        </tr>
                         </thead>
                         <tbody className="table-border-bottom-0">
-                            {renderRows()}
+                        {renderRows()}
                         </tbody>
                     </table>
                 </div>
             </div>
+
+            <Pagination tableResult={tableResult}/>
         </div>
     );
 }
---
+++ src/pages/layout/Pagination.jsx
@@ -0,0 +1,67 @@
+const Pagination = ({tableResult}) => {
+    return <nav aria-label="Page navigation" className="d-flex justify-content-center mx-auto gap-3 container-p-y">
+        <ul className="pagination">
+            {
+                !tableResult.first ?
+                    <li className="page-item first">
+                        <a className="page-link" href="javascript:void(0);">
+                            <i className="tf-icon bx bx-chevrons-left"></i>
+                        </a>
+                    </li> : ''
+            }
+            {
+                !((tableResult.number + 1 - 1) < 1) ?
+                    <li className="page-item prev">
+                        <a className="page-link" href="javascript:void(0);">
+                            <i className="tf-icon bx bx-chevron-left"></i>
+                        </a>
+                    </li> : ''
+            }
+            {
+                !((tableResult.number + 1 - 2) < 1) ?
+                    <li className="page-item">
+                        <a className="page-link" href="javascript:void(0);">{tableResult.number + 1 - 2}</a>
+                    </li> : ''
+            }
+            {
+                !((tableResult.number + 1 - 1) < 1) ?
+                    <li className="page-item">
+                        <a className="page-link" href="javascript:void(0);">{tableResult.number + 1 - 1}</a>
+                    </li> : ''
+            }
+            <li className="page-item active">
+                <a className="page-link" href="javascript:void(0);">{tableResult.number + 1}</a>
+            </li>
+            {
+                !((tableResult.number + 1 + 1) > tableResult.totalPages) ?
+                    <li className="page-item">
+                        <a className="page-link" href="javascript:void(0);">{tableResult.number + 1 + 1}</a>
+                    </li> : ''
+            }
+            {
+                !((tableResult.number + 1 + 2) > tableResult.totalPages) ?
+                    <li className="page-item">
+                        <a className="page-link" href="javascript:void(0);">{tableResult.number + 1 + 2}</a>
+                    </li> : ''
+            }
+            {
+                !((tableResult.number + 1 + 1) > tableResult.totalPages) ?
+                    <li className="page-item next">
+                        <a className="page-link" href="javascript:void(0);">
+                            <i className="tf-icon bx bx-chevron-right"></i>
+                        </a>
+                    </li> : ''
+            }
+            {
+                !tableResult.last ?
+                    <li className="page-item last">
+                        <a className="page-link" href="javascript:void(0);">
+                            <i className="tf-icon bx bx-chevrons-right"></i>
+                        </a>
+                    </li> : ''
+            }
+        </ul>
+    </nav>;
+}
+
+export default Pagination;
